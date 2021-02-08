3f1ed67449841be7dcb3729c50b6de5a2847d961
WTomko <WendyTomko@users.noreply.github.com>
2021-02-07 23:46:08 -0800

    change1
    2349pst
    7FEB21

--- index.js
+++ index.js
@@ -3,10 +3,24 @@
 var $ = function(id) {
 	return document.getElementById(id);
 }
+//
+//var downLoad = function() {
+//	alert("You have VERY SLOW internet. This is going to take a while. I suggest you take a 15 min Yoga break and then go get coffee!");
+//};
+//
+//$('download-boundaries').onclick = downLoad;
 
-var downLoad = function() {
-	alert("You have VERY SLOW internet. This is going to take a while. I suggest you take a 15 min Yoga break and then go get coffee!");
-};
 
-$("download-boundaries").onclick = downLoad;
+$(document).ready(function() {
+var $easyzoom = $('.easyzoom').easyZoom();
+});
 
+function circleofdeath() {
+	$("alert").style.display ="block";
+}
+
+function diecircleofdeath() {
+	$("alert").style.display = "none";
+}
+
+//blah blah
